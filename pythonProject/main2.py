1060ce1ed9faeb8619f2432bb3ce5687e4308617
Sergey-Shinkevich <sergey.shinkevich@hotmail.com>
2024-06-24 20:39:27 +0700

    ...

---
+++ pythonProject/main2.py
@@ -0,0 +1,46 @@
+# Задача "Длина слова"
+a = 'Hello World'
+print('---------------------------')
+print('Результат задачи ДЛИНА СЛОВА')
+print('Введенная строка:', a)
+print('Длина строки:', len(a), 'символов')
+print('---------------------------')
+
+# Задача "Суммы и разности"
+first = 20
+second = 30
+summa = first+second
+div = first-second
+print('Результат задачи СУММЫ И РАЗНОСТИ')
+print('Первое число =', first)
+print('Второе число =', second)
+print('Сумма =', summa)
+print('Разность =', div)
+print('---------------------------')
+
+# Задача "Среднее арифметическое"
+first = 20
+second = 50
+third = 70
+mean = (first+second+third)//3
+print('Результат задачи СРЕДНЕЕ АРИФМЕТИЧЕСКОЕ')
+print('First =', first)
+print('Second =', second)
+print('Third =', third)
+print('Mean =', mean)
+print('---------------------------')
+
+# Задача "Простые строчки"
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print('Результат задачи ПРОСТЫЕ СТРОЧКИ')
+print(second_string, 'и', first_string)
+print('---------------------------')
+
+# Задача "Сложная формула"
+a = 2
+b = 5
+c = 7
+f = (((a*b)+(a*c))**3)/2
+print('Результат задачи СЛОЖНАЯ ФОРМУЛА')
+print(f)
