40d54e042c2fce0c425757c41438c153403d8268
Sergey-Shinkevich <sergey.shinkevich@hotmail.com>
2024-06-25 23:17:52 +0700

    Практическое задание по теме "Переменные."

---
+++ pythonProject/main4.py
@@ -0,0 +1,6 @@
+print('Практическое задание по теме "Переменные."')
+number_of_homework_completed = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_per_task = number_of_hours_spent/number_of_homework_completed
+print('Курс:', course_name+',', 'всего задач:' + str(number_of_homework_completed)+',', 'затрачено часов:', str(number_of_hours_spent)+',', 'среднее время выполнения', time_per_task, 'часа')
