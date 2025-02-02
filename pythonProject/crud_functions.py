29ac66852750a5d13256a4bcadfd0bde45f31262
Sergey-Shinkevich <sergey.shinkevich@hotmail.com>
2025-02-02 11:41:05 +0700

    Add files via upload

--- pythonProject/crud_functions.py
+++ pythonProject/crud_functions.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect('database2.db')
 cursor = connection.cursor()
 
 cursor.execute('''
@@ -12,13 +12,37 @@ price INTEGER NOT NULL
 )
 ''')
 
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER NOT NULL,
+balance INTEGER NOT NULL
+)
+''')
+
 def get_all_products():
     cursor.execute("SELECT * FROM Products")
+    connection.commit()
     return cursor.fetchall()
 
 #for i in range(1,5):
 #    cursor.execute("INSERT INTO Products (title, description, price) VALUES (?, ?, ?)",
 #                   (f"Продукт {i}", f"Описание {i}",i * 100))
 
+def add_user(username, email, age):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)", (username, email, age, "1000"))
+    connection.commit()
+
+def is_included(username):
+    cursor.execute("SELECT username FROM Users WHERE username=?", (username,))
+    users = cursor.fetchall()
+    if users == []:
+        connection.commit()
+        return False
+    else:
+        connection.commit()
+        return True
+
 connection.commit()
-#connection.close()
